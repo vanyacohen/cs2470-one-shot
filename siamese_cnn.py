5a88887cbabe84e9aa8075a270374d3b121925f9
Vanya Cohen <vcohen@Vanyas-MacBook-Pro.local>
2017-11-17 18:53:43 -0500

    made siamese.

--- siamese_cnn.py
+++ siamese_cnn.py
@@ -4,38 +4,44 @@ import tensorflow as tf
 bSz = 128
 imgSz = 105
 
-imgBatch = tf.placeholder(tf.float32, [bSz, imgSz, imgSz, 1])
-conv1 = tf.layers.conv2d(
-    inputs=imgBatch,
-    filters=64,
-    kernel_size=[10, 10],
-    padding="valid",
-    activation=tf.nn.relu)
-pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
-conv2 = tf.layers.conv2d(
-    inputs=pool1,
-    filters=128,
-    kernel_size=[7, 7],
-    padding="valid",
-    activation=tf.nn.relu)
-pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-conv3 = tf.layers.conv2d(
-    inputs=pool2,
-    filters=128,
-    kernel_size=[4, 4],
-    padding="valid",
-    activation=tf.nn.relu)
-pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], strides=2)
-conv4 = tf.layers.conv2d(
-    inputs=pool3,
-    filters=256,
-    kernel_size=[4, 4],
-    padding="valid",
-    activation=tf.nn.relu)
+imgBatchA = tf.placeholder(tf.float32, [bSz, imgSz, imgSz, 1])
+imgBatchB = tf.placeholder(tf.float32, [bSz, imgSz, imgSz, 1])
 
-conv4_flat = tf.reshape(conv4, [bSz, 6 * 6 * 256])
-feature_vector = tf.layers.dense(inputs=conv4_flat, units=4096, activation=tf.nn.sigmoid)
-logits = tf.layers.dense(inputs=feature_vector, units=1, activation=tf.nn.sigmoid)
+def cnn(imgBatch):
+    conv1 = tf.layers.conv2d(
+        inputs=imgBatch,
+        filters=64,
+        kernel_size=[10, 10],
+        padding="valid",
+        activation=tf.nn.relu)
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=128,
+        kernel_size=[7, 7],
+        padding="valid",
+        activation=tf.nn.relu)
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+    conv3 = tf.layers.conv2d(
+        inputs=pool2,
+        filters=128,
+        kernel_size=[4, 4],
+        padding="valid",
+        activation=tf.nn.relu)
+    pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], strides=2)
+    conv4 = tf.layers.conv2d(
+        inputs=pool3,
+        filters=256,
+        kernel_size=[4, 4],
+        padding="valid",
+        activation=tf.nn.relu)
+
+    conv4_flat = tf.reshape(conv4, [bSz, 6 * 6 * 256])
+    feature_vector = tf.layers.dense(inputs=conv4_flat, units=4096, activation=tf.nn.sigmoid)
+    return feature_vector
+
+L1_distance_vector = tf.abs(tf.subtract(cnn(imgBatchA), cnn(imgBatchB)))
+logits = tf.layers.dense(inputs=L1_distance_vector, units=1, activation=tf.nn.sigmoid)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
